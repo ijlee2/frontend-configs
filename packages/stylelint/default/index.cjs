7b70e98f35d3c9134e1c6230f4404fd8223239fe
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-02-06 16:06:38 +0100

    Generalized stylelint configuration (#5)
    * refactor: Generalized configuration
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

---
+++ packages/stylelint/default/index.cjs
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
+
+    'declaration-block-no-redundant-longhand-properties': [
+      true,
+      {
+        ignoreShorthands: ['grid-gap', 'grid-template'],
+      },
+    ],
+  },
+};
