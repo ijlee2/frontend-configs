0e5141c311fa1e755babd0048a5d751acd509ca5
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-06 15:31:49 +0100

    chore: Consumed my-v2-addon

---
+++ tests/my-app/tests/integration/components/welcome-1-v2-test.ts
@@ -0,0 +1,18 @@
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupRenderingTest } from 'my-app/tests/helpers';
+import { module, test } from 'qunit';
+
+module('Integration | Component | welcome-1-v2', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<Welcome-1-V2 />`);
+
+    assert
+      .dom()
+      .hasText(
+        'Thanks for trying out create-v2-addon-repo. You can use my-app to document your packages.',
+      );
+  });
+});
