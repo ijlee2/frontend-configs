5239bbb24423e0d400e4f0f3e5080d2eafb3cf92
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-07 11:38:47 +0100

    chore: Updated project configurations

--- tests/my-v2-addon/eslint.config.mjs
+++ tests/my-v2-addon/eslint.config.mjs
@@ -1,3 +1,1 @@
-import eslintConfigEmberV2Addon from '@ijlee2-frontend-configs/eslint-config-ember/v2-addon';
-
-export default eslintConfigEmberV2Addon;
+export { default } from '@ijlee2-frontend-configs/eslint-config-ember/v2-addon';
