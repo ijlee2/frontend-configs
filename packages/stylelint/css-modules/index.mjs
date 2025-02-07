30e4b2a193a2826eb279bff8e45caa8a85af3d45
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-07 11:38:28 +0100

    feature: Supported esm

---
+++ packages/stylelint/css-modules/index.mjs
@@ -0,0 +1,60 @@
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
+
+    'declaration-block-no-duplicate-properties': [
+      true,
+      {
+        ignoreProperties: [
+          // Defined by CSS modules
+          'composes',
+        ],
+      },
+    ],
+
+    'declaration-block-no-redundant-longhand-properties': [
+      true,
+      {
+        ignoreShorthands: ['grid-gap', 'grid-template'],
+      },
+    ],
+
+    'order/properties-order': [
+      [
+        // Defined by CSS modules
+        'composes',
+      ],
+      {
+        unspecified: 'bottomAlphabetical',
+      },
+    ],
+
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: [
+          // Defined by CSS modules
+          'composes',
+        ],
+      },
+    ],
+
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: [
+          // Defined by CSS modules
+          'global',
+        ],
+      },
+    ],
+  },
+};
---
+++ packages/stylelint/default/index.mjs
@@ -0,0 +1,20 @@
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
+
+    'declaration-block-no-redundant-longhand-properties': [
+      true,
+      {
+        ignoreShorthands: ['grid-gap', 'grid-template'],
+      },
+    ],
+  },
+};
