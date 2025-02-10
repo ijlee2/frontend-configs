ac81e38dc3fecadfaa22504d89735a81014a51ef
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-02-10 10:09:30 +0100

    Reverted #7 (#13)
    * Revert "Adopted ember-template-lint's plugin system (#7)"
    
    This reverts commit a62f48788aab41c312cb94233e5f795b5914e7c9.
    
    * chore: Updated README
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/ember-template-lint/index.cjs
+++ packages/ember-template-lint/index.cjs
@@ -1,30 +1,21 @@
 'use strict';
 
 module.exports = {
-  name: '@ijlee2-frontend-configs/ember-template-lint',
-  configurations: {
-    recommended: {
-      plugins: ['ember-template-lint-plugin-prettier'],
-      extends: [
-        'recommended',
-        'ember-template-lint-plugin-prettier:recommended',
-      ],
-      overrides: [
-        {
-          files: ['**/*.{gjs,gts}'],
-          rules: {
-            'no-implicit-this': 'off',
-            prettier: 'off',
-          },
-        },
-        {
-          files: ['tests/**/*-test.{js,ts}'],
-          rules: {
-            prettier: 'off',
-          },
-        },
-      ],
+  plugins: ['ember-template-lint-plugin-prettier'],
+  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  overrides: [
+    {
+      files: ['**/*.{gjs,gts}'],
+      rules: {
+        'no-implicit-this': 'off',
+        prettier: 'off',
+      },
     },
-  },
-  rules: {},
+    {
+      files: ['tests/**/*-test.{js,ts}'],
+      rules: {
+        prettier: 'off',
+      },
+    },
+  ],
 };
