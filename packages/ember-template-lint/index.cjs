660368092c2eeb9a086458befe59c6a6b4a8f94e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-05-13 08:35:20 +0200

    feature: Enabled sort-invocations

--- packages/ember-template-lint/index.cjs
+++ packages/ember-template-lint/index.cjs
@@ -3,6 +3,9 @@
 module.exports = {
   plugins: ['ember-template-lint-plugin-prettier'],
   extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  rules: {
+    'sort-invocations': true,
+  },
   overrides: [
     {
       files: ['**/*.{gjs,gts}'],
