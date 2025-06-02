5a53484281206541b9bdd79c4825bd1b40318d76
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-06-02 05:40:26 +0200

    breaking: Removed ember-template-lint-plugin-prettier

--- packages/ember-template-lint/index.cjs
+++ packages/ember-template-lint/index.cjs
@@ -1,23 +1,8 @@
 'use strict';
 
 module.exports = {
-  plugins: ['ember-template-lint-plugin-prettier'],
-  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  extends: ['recommended'],
   rules: {
     'sort-invocations': true,
   },
-  overrides: [
-    {
-      files: ['**/*.{gjs,gts}'],
-      rules: {
-        prettier: 'off',
-      },
-    },
-    {
-      files: ['tests/**/*-test.{js,ts}'],
-      rules: {
-        prettier: 'off',
-      },
-    },
-  ],
 };
