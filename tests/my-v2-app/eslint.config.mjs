177421edfe2b48f78f55d0805308274d32246c92
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-06-05 05:51:03 +0200

    chore: Temporarily ignored 3 files in tests/my-v2-app

--- tests/my-v2-app/eslint.config.mjs
+++ tests/my-v2-app/eslint.config.mjs
@@ -1,1 +1,13 @@
-export { default } from '@ijlee2-frontend-configs/eslint-config-ember/v2-app';
+import baseConfiguration from '@ijlee2-frontend-configs/eslint-config-ember/v2-app';
+
+export default [
+  ...baseConfiguration,
+  // Cannot find module '/node_modules/.pnpm/@embroider+macros@1.18.0_@glint+template@1.5.2/node_modules/@embroider/macros/src/glimmer/ast-transform.js'
+  {
+    ignores: [
+      'config/environment.js',
+      'config/targets.js',
+      'ember-cli-build.js',
+    ],
+  },
+];
