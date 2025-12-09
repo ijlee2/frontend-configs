c1a43740fdaf7a4665f757b8570ba889683e81a2
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-09 16:29:32 +0100

    Renamed function

--- .changeset/formatter.mjs
+++ .changeset/formatter.mjs
@@ -1,3 +1,3 @@
-import getConfig from '@ijlee2-frontend-configs/changesets';
+import getFormatter from '@ijlee2-frontend-configs/changesets';
 
-export default getConfig('ijlee2/frontend-configs');
+export default getFormatter('ijlee2/frontend-configs');
