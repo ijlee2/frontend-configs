40257c554804b563d643422f3c8bb81ba53f34b7
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-29 09:34:52 +0100

    Updated dependencies (#74)
    * chore: Updated dependencies
    
    * chore: Downstreamed changes to v2 apps
    
    * chore: Added lockfile
    
    * chore: Added changeset

--- tests/my-v2-app/eslint.config.mjs
+++ tests/my-v2-app/eslint.config.mjs
@@ -1,13 +1,1 @@
-import baseConfiguration from '@ijlee2-frontend-configs/eslint-config-ember/v2-app';
-
-export default [
-  ...baseConfiguration,
-  // Cannot find module '/node_modules/.pnpm/@embroider+macros@1.18.0_@glint+template@1.5.2/node_modules/@embroider/macros/src/glimmer/ast-transform.js'
-  {
-    ignores: [
-      'config/environment.js',
-      'config/targets.js',
-      'ember-cli-build.js',
-    ],
-  },
-];
+export { default } from '@ijlee2-frontend-configs/eslint-config-ember/v2-app';
